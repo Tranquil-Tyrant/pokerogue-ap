1caaeda1d0fc00d503769d50b110dbb01e092d2a
Tranquil Tyrant <tranquil.tyrant@gmail.com>
2026-07-19 13:38:25 -0700

    Initial launch of fully integrated Archipelago web fork

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pokerogue.archipelago',
+  appName: 'pokerogue archipelago',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ src/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pokerogue.archipelago',
+  appName: 'PokeRogue Archipelago',
+  webDir: 'dist'
+};
+
+export default config;
